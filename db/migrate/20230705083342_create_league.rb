9fd2bc94fb0c751461bfbeff384e223393481be4
Stevie <kamaustevek78@gmail.com>
2023-07-05 11:43:10 +0300

    Database Updated

---
+++ db/migrate/20230705083342_create_league.rb
@@ -0,0 +1,10 @@
+class CreateLeague < ActiveRecord::Migration[7.0]
+  def change
+    create_table :leagues do |t|
+      t.references :player, null: false, foreign_key: true
+      t.references :team, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
